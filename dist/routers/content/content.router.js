7577b2281e5d3c6cdf6918a1d25598721f494383
karand07 <karandanole07@gmail.com>
2026-02-07 11:55:44 +0530

    get content route created

--- dist/routers/content/content.router.js
+++ dist/routers/content/content.router.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import userAuth from "../../middleware/user.middleware.js";
 import { contentModel } from "../../model/content.model.js";
 import { contentSchema } from "./content.util.js";
+import { json } from "zod";
 const contentRoute = Router();
 contentRoute.use(userAuth);
 contentRoute.post('/post', async (req, res) => {
@@ -24,6 +25,26 @@ contentRoute.post('/post', async (req, res) => {
         res.status(200).json({
             message: "content created successfully",
             id: content._id
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            message: "internal server error",
+            error: error
+        });
+    }
+});
+contentRoute.get('/get', async (req, res) => {
+    try {
+        const userId = req.userId;
+        if (!userId) {
+            return res.json("token expired");
+        }
+        const contents = await contentModel.find({
+            user: userId
+        });
+        res.status(200).json({
+            contents
         });
     }
     catch (error) {
--- src/routers/content/content.router.ts
+++ src/routers/content/content.router.ts
@@ -2,6 +2,7 @@ import { Router, type Request, type Response } from "express";
 import userAuth from "../../middleware/user.middleware.js";
 import { contentModel } from "../../model/content.model.js";
 import { contentSchema } from "./content.util.js";
+import { json } from "zod";
 
 const contentRoute = Router();
 
@@ -44,4 +45,25 @@ contentRoute.post
     
 })
 
+contentRoute.get('/get',
+    async (req:Request,res:Response) => {
+    try {
+        const userId = req.userId;
+        if (!userId){
+            return res.json("token expired")
+        }
+        const contents = await contentModel.find({
+            user:userId
+        })
+
+        res.status(200).json({
+            contents
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message:"internal server error",
+            error:error
+        })
+    }
+})
 export {contentRoute}
