f7334690b1ac14535c739f3800dd127b05c6b391
karand07 <karandanole07@gmail.com>
2026-03-16 19:41:34 +0530

    SETTING UP ENVIRONMENT VARIABLES

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,9 @@ const app = express()
 const mongoUrl=process.env.MONGO_URL || ''
 app.use(cors(
   {
-    origin: 'http://localhost:5173', // Replace with your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
+    origin: process.env.FRONTEND_URL , 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization']
   }
 ));
 
