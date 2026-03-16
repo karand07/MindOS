fe8bca5337fcbcceed023fd7c095a8a2bf8e6de4
karand07 <karandanole07@gmail.com>
2026-03-16 12:52:58 +0530

    improved link model

--- src/model/links.model.ts
+++ src/model/links.model.ts
@@ -3,28 +3,23 @@ import {model, Schema,Types} from "mongoose";
 interface ILink{
     hash:String,
     user:Types.ObjectId
-    createdAt:Date
 }
 
-const linkSchema = new Schema <ILink>({
-    hash:{
-        type:String,
-        required:true,
-    },
-    user:{
-        type:Schema.Types.ObjectId,
-        ref:"user" ,
-        unique:true,
-        required:true
-    },
-    createdAt: {
-    type: Date,
-    default: Date.now,
-    expires: 3600 
+const linkSchema = new Schema<ILink>({
+  hash: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    unique: true,
+    required: true
   }
 },{
-    timestamps:true
-    }
-)
+  timestamps: true
+});
+
+linkSchema.index({ createdAt: 1 }, { expireAfterSeconds: 3600 });
 
 export const linkModel = model<ILink>("link",linkSchema)
